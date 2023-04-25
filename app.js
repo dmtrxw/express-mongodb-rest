7529949109fd41d57cee3f0249a3d573d4078f54
Dimitri <gitmitri@gmail.com>
2023-04-25 18:26:47 +0700

    feat: cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 const { connect } = require('./db')
 const routes = require('./routes/')
 const errorHandler = require('./middlewares/errorHandler')
 
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
